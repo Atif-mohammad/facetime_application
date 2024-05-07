24cfd6452e9bb3ab3108bf4dd48266991155f820
Atif-mohammad <sy24604@gmail.com>
2024-05-07 20:12:19 +0530

    Meeting Room Successfully Created

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,14 @@
 import StreamVideoProvider from '@/providers/StreamClientProvider'
 import { StreamVideoClient } from '@stream-io/node-sdk'
 import React, { ReactNode } from 'react'
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: "facetime_application",
+  description: "A video Calling Application",
+  icons: '/icons/logo.svg'
+};
+
+
 const RootLayout = ({children} : {children : ReactNode}) => {
   return (
     <main>
